87f61b4fec57ce563f2305dd6ce7f1a7a220de8e
Jane <duddms72003@naver.com>
2024-02-09 16:52:18 +0900

    modify basePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  basePath: "/React-Modal-2023-challenge",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/React-Modal-2023-challenge" : "",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
